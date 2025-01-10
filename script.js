522ef5490f888e87018fd8ae850e7e5709db97c1
JinditthamCapitals <shashwatjdc@gmail.com>
2025-01-11 01:51:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Smooth scroll for anchor links (if needed later)
+window.onload = function(){
+    slide();
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', function () {
+        const navbar = document.querySelector('.navbar');
+        if (window.scrollY > 50) { // Adjust scroll threshold as needed
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
+    
